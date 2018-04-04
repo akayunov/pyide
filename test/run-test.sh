11372cc2a0c5f3699c7916fa2fdcae4f884db1d6
Alexey Kayunov <akayunov@intermedia.net>
2018-04-04 17:49:26 +0300

    #1

--- test/run-test.sh
+++ test/run-test.sh
@@ -1,17 +1,39 @@
 #!/usr/bin/env bash
 
-script_dir_name="`dirname \"$0\"`"              # relative
-PROJECT_DIR="`( cd \"${script_dir_name}/../\" && pwd )`"  # absolutized and normalized
-echo ${PROJECT_DIR}
+main() {
+    local cmd=$1
+    shift
 
-# lint tests
-docker run -it --rm -v ${PROJECT_DIR}:/opt/pyide:ro registry.hub.docker.com/akayunov/pyide-test:latest bash -c \
-'echo "Run pycodestyle" && pycodestyle                         /opt/pyide/src/pyide /opt/pyide/test/client   /opt/pyide/test/server/tests || \
- echo "Run pyflakes"    && pyflakes                            /opt/pyide/src/pyide /opt/pyide/test/client   /opt/pyide/test/server/tests || \
- echo "Run pylint"      && pylint --rcfile=/opt/pyide/pylintrc /opt/pyide/src/pyide /opt/pyide/test/client/* /opt/pyide/test/server/tests/*  \
-'
+    script_dir_name="`dirname \"$0\"`"              # relative
+    PROJECT_DIR="`( cd \"${script_dir_name}/../\" && pwd )`"  # absolutized and normalized
+    echo ${PROJECT_DIR}
 
+  case ${cmd} in
+    "all") test_lint && test_functional;;
+    "func") test_functional;;
+    "lint") test_lint;;
+    *) echo "Run as: $0 command
+Possible commands:
+  all     - run all tests
+  func    - run functional tests
+  lint    - run lint tests
+  "; exit;;
+  esac
+}
 
-# functional test
-docker run -it --rm -v "$PROJECT_DIR"/test:/opt/pyide/test:ro -v "$PROJECT_DIR"/tmp:/opt/pyide/tmp  --network=PYIDE_pyide \
-    registry.hub.docker.com/akayunov/pyide-test:latest pytest /opt/pyide/test/client
+test_lint(){
+    # lint tests
+    docker run -it --rm -v ${PROJECT_DIR}:/opt/pyide:ro registry.hub.docker.com/akayunov/pyide-test:latest bash -c \
+    'echo "Run pycodestyle" && pycodestyle                         /opt/pyide/src/pyide /opt/pyide/test/client   /opt/pyide/test/server/tests && \
+     echo "Run pyflakes"    && pyflakes                            /opt/pyide/src/pyide /opt/pyide/test/client   /opt/pyide/test/server/tests && \
+     echo "Run pylint"      && pylint --rcfile=/opt/pyide/pylintrc /opt/pyide/src/pyide /opt/pyide/test/client/* /opt/pyide/test/server/tests/*  \
+    '
+}
+
+test_functional(){
+    # functional test
+    docker run -it --rm -v "$PROJECT_DIR"/test:/opt/pyide/test:ro -v "$PROJECT_DIR"/tmp:/opt/pyide/tmp  --network=PYIDE_pyide \
+        registry.hub.docker.com/akayunov/pyide-test:latest pytest /opt/pyide/test/client
+}
+
+main "$@"
